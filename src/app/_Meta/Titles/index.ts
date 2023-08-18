2d8f06351b275dab144be5509d633777cca4779b
MSK1206 <phantom7216@yahoo.co.jp>
2023-08-19 00:38:06 +0900

    Add: Titles

---
+++ src/app/_Meta/Titles/index.ts
@@ -0,0 +1,21 @@
+// Brand Name
+export const BrandName = 'Sample'
+
+// All page titleTemplate
+export const TitleTemplate = BrandName
+
+// Root page.tsx title
+export const RootTitle = 'ホーム'
+export const RootDesc = 'Next.js + microCMSで作られたSampleのテンプレートです'
+
+// News page.tsx
+export const NewsTitle = 'ニュース'
+
+// Business page.tsx title
+export const BusinessTitle = '事業内容'
+
+// Recruit
+export const RecruitTitle = '採用情報'
+
+// Contact page.tsx title
+export const ContactTitle = 'お問い合わせ'
